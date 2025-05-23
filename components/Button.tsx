2ae1deb969c8ba56adc19d51810b3dedaf83f930
Pakorn Vongseela <18078609+pakornv@users.noreply.github.com>
2025-05-23 12:53:56 +0700

    feat: upgrade to fresh 2

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,16 @@
-import { JSX } from "preact";
-import { IS_BROWSER } from "$fresh/runtime.ts";
+import type { ComponentChildren } from "preact";
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+export interface ButtonProps {
+  onClick?: () => void;
+  children?: ComponentChildren;
+  disabled?: boolean;
+}
+
+export function Button(props: ButtonProps) {
   return (
     <button
       {...props}
-      disabled={!IS_BROWSER || props.disabled}
-      class="btn"
+      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
     />
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,12 @@
 import { useSignal } from "@preact/signals";
+import { define } from "../utils.ts";
 import Counter from "../islands/Counter.tsx";
 
-export default function Home() {
+export default define.page(function Home() {
   const count = useSignal(3);
+
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto fresh-gradient">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
@@ -22,4 +24,4 @@ export default function Home() {
       </div>
     </div>
   );
-}
+});
